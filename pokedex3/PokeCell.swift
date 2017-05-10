c6c45a4f9c7c27b168fc0d321433df3d99cfe8ed
Regis Zaleman <regis.zaleman@gmail.com>
2017-05-10 18:45:55 -0400

    Add assets

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Regis Zaleman on 5/10/17.
+//  Copyright © 2017 The Dalton School. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Regis Zaleman on 5/10/17.
+//  Copyright © 2017 The Dalton School. All rights reserved.
+//
+
+import Foundation
